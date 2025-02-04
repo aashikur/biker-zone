a2489f2eb9b82f9935d282891aebfb5bedb23e86
aashikur <mdaashikur@gmail.com>
2025-02-04 22:50:15 +0600

    Full navbar and Slider Done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'bike-primary': 'red', // Replace with your desired color
+        },
+      },
+    },
+    plugins: [],
+  };
+  
